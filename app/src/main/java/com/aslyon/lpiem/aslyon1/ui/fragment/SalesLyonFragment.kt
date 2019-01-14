edb0bf136d38595c068bb717aaaab9333368bd51
Unknown <sof.benzaied@gmail.com>
2019-01-14 17:29:56 +0100

    [FEAT] add UDL shop

---
+++ app/src/main/java/com/aslyon/lpiem/aslyon1/ui/fragment/SalesLyonFragment.kt
@@ -0,0 +1,47 @@
+package com.aslyon.lpiem.aslyon1.ui.fragment
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import com.aslyon.lpiem.aslyon1.R
+import kotlinx.android.synthetic.main.fragment_sales.*
+
+class SalesLyonFragment : BaseFragment(){
+
+    companion object {
+        const val TAG = "SALESLYONFRAGMENT"
+        fun newInstance(): SalesLyonFragment = SalesLyonFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sales, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        wv_sales_fragment.webViewClient=MyWebViewClient()
+        wv_sales_fragment.loadUrl("https://asudl.universite-lyon.fr/boutique-66434.kjsp?RH=1749694146834577&RF=1543530668610")
+
+
+    }
+
+    class MyWebViewClient : WebViewClient(){
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            view?.loadUrl("https://asudl.universite-lyon.fr/boutique-66434.kjsp?RH=1749694146834577&RF=1543530668610")
+            return true
+        }
+
+
+
+    }
+}
